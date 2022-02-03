a7054bd54ddc835ce02d82aeff0084dd836d6cf2
Kevin Remoue <kevin.remoue@gmail.com>
2022-02-03 10:11:17 +0800

    First commit with application

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from email import header
+from typing import Optional
+import socket
+
+from fastapi import FastAPI, Response
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+def generate_html_response():
+    hostname=socket.getfqdn()
+    html_content = """
+    <html>
+        <head>
+            <title>Webapp</title>
+        </head>
+        <body>
+            <h1>WebApp running</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200, headers={"X-Backend-Server":hostname})
+
+@app.get("/", response_class=HTMLResponse)
+def read_root():
+    return generate_html_response()
