bdd7fc4d50a97dba6c666daf40a13e94f3157001
Kevin Remoue <kevin.remoue@gmail.com>
2022-02-04 11:53:28 +0800

    adding EC2 instance

--- main.tf
+++ main.tf
@@ -2,7 +2,21 @@ provider "aws" {
 
 }
 
-resource "aws_ecr_repository" "kevinremoue" {
-  name = "kevinremoue"
-  image_tag_mutability = "MUTABLE"
+#get data
+data "aws_ecr_repository" "kevinremoue" {
+    name = "kevinremoue"
+}
+
+resource "aws_instance" "test" {
+  instance_type         = "t2.micro"
+  ami                   = "ami-06cffe063efe892ad"
+
+  user_data = <<-EOF
+    #!/bin/bash
+    set -ex
+    sudo yum update -y
+    sudo amazon-linux-extras install docker -y
+    sudo systemctl start docker
+    sudo usermod -a -G docker ec2-user
+  EOF
 }
