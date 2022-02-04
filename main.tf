3cd6266ed7741bb34c5267cedc576ccebbf92a69
Kevin Remoue <kevin.remoue@gmail.com>
2022-02-04 10:33:23 +0800

    Adding secrets and main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region     = "${{ secrets.AWS_DEFAULT_REGION }}"
+    access_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"
+    secret_key = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
+
+    # assume_role {
+    #     role_arn     = "arn:aws:iam::ACCOUNT_ID:role/ROLE_NAME"
+    #     session_name = "SESSION_NAME"
+    #     external_id  = "EXTERNAL_ID"
+    # }
+}
